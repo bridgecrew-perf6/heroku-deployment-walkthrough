31ceab0b35b180b19c7934b6028d96a3050979e1
krabecb <60526718+krabecb@users.noreply.github.com>
2022-02-10 15:29:16 -0600

    Connected to db

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT
 
+require('./db/db')
+
 app.listen(PORT, () => {
     const d = new Date()
     console.log(`${d.toLocaleDateString()}: Server is running on port: ${PORT}🙌🏽`)
