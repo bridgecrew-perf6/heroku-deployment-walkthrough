9a83aa9155bb22730d4bd11d3e4453dac2682063
krabecb <60526718+krabecb@users.noreply.github.com>
2022-02-11 15:14:02 -0600

    Installed cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT
+
+app.use(cors())
 
 require('./db/db')
 const Show = require('./models/show')
